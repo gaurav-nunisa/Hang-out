6849c695dcd83f12b99399e37769450fca0bdfdf
RoninNunisa <144159717+RoninNunisa@users.noreply.github.com>
2024-10-21 01:50:28 +0530

    v1.2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import Home from "./pages/home/home";
-import SignUp from "./pages/signup/signup";
-import Login from "./pages/login/login";
+import Home from "./pages/home/Home";
+import SignUp from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
 import { Navigate,Routes, Route} from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
